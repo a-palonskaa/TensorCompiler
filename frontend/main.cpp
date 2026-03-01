44c4705d469537016ff8ea2c7687bb63c7f72f8b
Alina <alinapalonskaa0@gmail.com>
2026-03-01 23:05:28 +0300

    [FRONT-5] fucking ci

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -20,32 +20,37 @@ std::string replace_extension(const std::string& filename,
 }  // namespace
 
 namespace Config {
-    constexpr char LOGS_DIR[] = "./logs/";
-    constexpr char IMG_DIR[] = "./images/";
-    constexpr char MODELS[] = "./models/";
+constexpr char LOGS_DIR[] = "./logs/";
+constexpr char IMG_DIR[] = "./images/";
+constexpr char MODELS[] = "./models/";
 
-    constexpr char DOT_EXT[] = ".dot";
-    constexpr char PNG_EXT[] = ".png";
-    constexpr char LOG_EXT[] = ".log";
+constexpr char DOT_EXT[] = ".dot";
+constexpr char PNG_EXT[] = ".png";
+constexpr char LOG_EXT[] = ".log";
 
-    constexpr char LOGFILE[] = "frontend.log";
-}
+constexpr char LOGFILE[] = "frontend.log";
+}  // namespace Config
 
 int main(int argc, const char* argv[]) {
-    if ((argc < 2) || (argc > 3) || (argc == 3 && argv[2] && strcmp(argv[2], "-d") != 0)) {
-        std::cerr << "Usage: " << argv[0] << " <model.onnx>" << "[optional -d]" << '\n';
+    if ((argc < 2) || (argc > 3) ||
+        (argc == 3 && argv[2] && strcmp(argv[2], "-d") != 0)) {
+        std::cerr << "Usage: " << argv[0] << " <model.onnx>" << "[optional -d]"
+                  << '\n';
         return 0;
     }
 
-    if (!Logger::getInstance().setLogFile(std::string(Config::LOGS_DIR)+std::string(Config::LOGFILE))) {
+    if (!Logger::getInstance().setLogFile(std::string(Config::LOGS_DIR) +
+                                          std::string(Config::LOGFILE))) {
         LOG(INFO, "logs will be printed to std::cerr\n");
     }
 
     std::filesystem::create_directories(Config::LOGS_DIR);
     std::filesystem::create_directories(Config::IMG_DIR);
 
-    std::string dot_filename = Config::IMG_DIR + replace_extension(argv[1], Config::DOT_EXT);
-    std::string png_filename = Config::IMG_DIR + replace_extension(argv[1], Config::PNG_EXT);
+    std::string dot_filename =
+        Config::IMG_DIR + replace_extension(argv[1], Config::DOT_EXT);
+    std::string png_filename =
+        Config::IMG_DIR + replace_extension(argv[1], Config::PNG_EXT);
 
     TensorCompiler::ONNXParser parser;
     std::string path = std::string(Config::MODELS) + argv[1];
@@ -67,7 +72,8 @@ int main(int argc, const char* argv[]) {
     std::string cmd = "dot -Tpng " + dot_filename + " -o " + png_filename;
     int ret = std::system(cmd.c_str());
     if (ret != 0) {
-        LOG(WARNING, "Failed to render PNG (dot returned " + std::to_string(ret) + ").\n");
+        LOG(WARNING, "Failed to render PNG (dot returned " +
+                         std::to_string(ret) + ").\n");
     } else {
         std::cout << "\nGraph rendered to " << png_filename << '\n';
     }
